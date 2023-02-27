a32113ab29c6f94e1115283d73397b93c1e17b6e
Alex-M-E <99903206+Alex-M-E@users.noreply.github.com>
2023-02-27 23:02:06 +0300

    Update gameteleg.py

--- bot/gameteleg.py
+++ bot/gameteleg.py
@@ -4,48 +4,48 @@ import json
 import random as rd
 import time
 
-with open('gamet.json') as f:
+with open('gamet.json', encoding='windows-1251') as f:
     data = json.load(f)
 a = data['level']
+ff = {}
+for i in range(len(data[a]['togo'])):
+    ff['/' + str(i)] = data[a]['togo'][i]
 
-bot = telebot.TeleBot('secret token')
+bot = telebot.TeleBot('5890454132:AAG0Ad9szAx3jEdkxV7vJaJo6NJftKnwI0g')
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-        global a
-        global data
-        if m.text == "/start":
-            print('Кто-то начал игру')
-            with open('gamet.json') as f:
-                data = json.load(f)
-                a = data['level'] 
-            bot.send_message(m.from_user.id, data[a]['text'])
-            for i in data[a]['togo']:
-                bot.send_message(m.from_user.id, i + " - " + data[a][i])
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("/start")
-        item2=types.KeyboardButton("/rule")
-        markup.add(item1)
-        markup.add(item2)
-        bot.send_message(m.from_user.id, 'Выберай пункт',  reply_markup=markup)
+    global a
+    global data
+    global ff
+    if m.text == "/start":
+        print('Кто-то начал игру')
+        with open('gamet.json', encoding='windows-1251') as f:
+            data = json.load(f)
+            a = data['level']
+            ff = {}
+            for i in range(len(data[a]['togo'])):
+                ff['/' + str(i)] = data[a]['togo'][i]
+        bot.send_message(m.from_user.id, data[a]['text'])
+        for i in ff:
+            bot.send_message(m.from_user.id, i + " - " + data[a][ff[i]])
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("/start")
+    item2=types.KeyboardButton("/rule")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(m.from_user.id, 'Выберай пункт',  reply_markup=markup)
       
     
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global a
     global data
-    if message.text == "/start":
-        with open('gamet.json') as f:
-            data = json.load(f)
-            a = data['level'] 
-        bot.send_message(message.from_user.id, data[a]['text'])
-        for i in data[a]['togo']:
-            bot.send_message(message.from_user.id, i + " - " + data[a][i])
-        return None
-    elif message.text == '/rule':
+    global ff
+    if message.text == '/rule':
         bot.send_message(message.from_user.id, data['ruels'])
-    elif message.text in data[a]["togo"]:
-        a = message.text
+    elif message.text in ff:
+        a = ff[message.text]
         bot.send_message(message.from_user.id, data[a]['text'])
         if 'kill' in data[a]:
             d = data['kill' + data[a]['kill']]
@@ -56,11 +56,16 @@ def get_text_messages(message):
             if data[a]['togo'] == []:
                 bot.send_message(message.from_user.id, data[a]['text'])
                 bot.send_message(message.from_user.id, data[a]['st'])
+                print('Пользователь закончил игру')
                 return None
         elif data[a]['togo'] == []:
             bot.send_message(message.from_user.id, data[a]['st'])
-        for i in data[a]['togo']:
-            bot.send_message(message.from_user.id, i + " - " + data[a][i])
+            print('Пользователь закончил игру')
+        ff = {}
+        for i in range(len(data[a]['togo'])):
+            ff['/' + str(i)] = data[a]['togo'][i]
+        for i in ff:
+            bot.send_message(message.from_user.id, i + " - " + data[a][ff[i]])
         return None     
     else:
         bot.send_message(message.from_user.id, "Чо?")
